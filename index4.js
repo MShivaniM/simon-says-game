d08b76272f71484f6b4193a80e4e3eb45237c1e8
ShivaniMuddamwar <shivani220298@gmail.com>
2024-04-09 19:13:09 +0530

    simon says game

---
+++ index4.js
@@ -0,0 +1,46 @@
+// const max = prompt("Enter max number");
+
+// const random = Math.floor(Math.random()*max) + 1
+
+// let guess = prompt("guess the number");
+
+// while(true){
+//     if( guess == "quit"){
+//         console.log("You quit");
+//         break;
+//     }
+//     if(guess == random){
+//         console.log("You are right! congrats!! random number was", random);
+//         break;
+//     } else if(guess < random){
+//         console.log("hint: Your number is too small. Please try again");
+//         guess = prompt("guess the number");
+//     } else {
+//         console.log("hint: Your number is too large. Please try again");
+//         guess = prompt("guess the number");
+//     }
+// } 
+
+
+// dice roll
+
+// let dice = Math.floor(Math.random()*6) + 1;
+// console.log(random);
+
+// const car = {
+//     name: "Maruti Suzuki",
+//     model: "Dzire",
+//     color: "white"
+// };
+// console.log(car.name);
+
+// const person = {
+//     name: "Shradha",
+//     age: 23,
+//     city: "Delhi"
+// }
+
+// person.city = "New York";
+// person.country = "USA";
+
+// console.log(person);
